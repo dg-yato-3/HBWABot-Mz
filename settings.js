eeaa0bb77f201a54d5cec3530ac73990f45b01d6
dg-yato-3 <lalmesaka58@gmail.com>
2024-09-09 19:06:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
+global.AntiCallBot = false // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
 global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
@@ -12,22 +12,22 @@ global.autoread = true // true a i dah chuan message zawng zawng read vek zelin 
 global.autoread_status = true // true a i dah chuan auto status view in a awm ang
 
 //info
-global.owner = ['918416093656'] // I phone number in thlak rawh
-global.ownernumber = '918416093656' // I phone number in thlak rawh
-global.ownername = "꧁Herbert♕꧂" // I hming dah tel bawk la
+global.owner = ['916909004540'] // I phone number in thlak rawh
+global.ownernumber = '916909004540' // I phone number in thlak rawh
+global.ownername = "celine" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
-global.botname = "HBWABot Mizo" // I bot hming tur dah la
+global.botname = "celine" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
-global.creator = "918416093656@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
+global.creator = "916909004540@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["918416093656@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.ownerNumber = ["916909004540@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
 global.prefixc = ['/'] // Prefix a tana hman theih loh tur symbolise a awm nuala, i bot a work a lo a nih chuan i thlak mai dawn nia
 global.sessionName = 'session'
 
 // Sticker Pack Name
-global.packname = "Sticker By"
-global.author = "HBWABot"
+global.packname = "Sticker By Celine"
+global.author = "Celine"
 
 //BUTTON INPUT 
 global.buttonyt = "SUBSCRIBE"
